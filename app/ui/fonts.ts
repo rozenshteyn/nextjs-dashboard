830eff2a5553c1b8036b0b7f4910293560596329
Roman Rozenshteyn <rozenshteyn@yahoo.com>
2024-05-05 09:59:56 -0700

    Use Next.js layout and links.  Use Google fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Roboto }  from "next/font/google";
+
+export const lucitana = Lusitana({ weight: ["400"], subsets: ["latin"] });
+export const roboto = Roboto({ weight: ["700"], subsets: ["latin"] });
