3cc552ddea1133ff0500337a8ea6aca320564b8e
Roman Rozenshteyn <rozenshteyn@yahoo.com>
2024-05-05 10:27:41 -0700

    Fix the spelling of the font name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from "@/app/ui/home.module.css";
-import { lucitana } from "@/app/ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function Page() {
   return (
@@ -16,7 +16,7 @@ export default function Page() {
           <div
             className={styles.shape}
           />
-          <p className={`${lucitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Lusitana, Roboto }  from "next/font/google";
 
-export const lucitana = Lusitana({ weight: ["400"], subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: ["400"], subsets: ["latin"] });
 export const roboto = Roboto({ weight: ["700"], subsets: ["latin"] });
