7497dc966a45d28f1679581e7318db0fa6a0d5dc
Roman Rozenshteyn <rozenshteyn@yahoo.com>
2024-05-26 22:02:41 -0700

    Add functionality to handle errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,17 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    }
+    catch (error) {
+        return {
+            message: "Database error: failed to create an invoice"
+        }
+    }
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
@@ -44,21 +51,35 @@ export async function updateInvoice(id: string, formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}, date = ${date}
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}, date = ${date}
+            WHERE id = ${id}
+        `;
+    }
+    catch (error) {
+        return {
+            message: "Database error: failed to update an invoice"
+        }
+    }
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`
-        DELETE FROM invoices
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            DELETE FROM invoices
+            WHERE id = ${id}
+        `;
+    }
+    catch (error) {
+        return {
+            message: "Database error: failed to delete an invoice"
+        }
+    }
 
     revalidatePath("/dashboard/invoices");
 }
