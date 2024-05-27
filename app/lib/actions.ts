f90bacc8bca48acd6b7cf77b1dd3e950b67e0707
Roman Rozenshteyn <rozenshteyn@yahoo.com>
2024-05-27 09:48:49 -0700

    Add form data validation with accessible error messages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,21 +7,42 @@ import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(["pending", "paid"]),
+    customerId: z.string({
+        invalid_type_error: "Please select a customer."
+    }),
+    amount: z.coerce.number().gt(0, { message: "Please enter an amount greater than $0" }),
+    status: z.enum(["pending", "paid"], {
+        invalid_type_error: "Please select an invoice status."
+    }),
     date: z.string(),
 });
 
 const InvoiceParser = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = InvoiceParser.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[]
+    };
+    message?: string | null
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = InvoiceParser.safeParse({
         customerId: formData.get("customerId"),
         amount: formData.get("amount"),
         status: formData.get("status")
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing fields.  Failed to create an invoice"
+        }
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
 
@@ -41,12 +62,21 @@ export async function createInvoice(formData: FormData) {
     redirect("/dashboard/invoices");
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = InvoiceParser.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = InvoiceParser.safeParse({
         customerId: formData.get("customerId"),
         amount: formData.get("amount"),
         status: formData.get("status")
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing fields.  Failed to update the invoice"
+        }
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
 
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
@@ -60,7 +90,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     }
     catch (error) {
         return {
-            message: "Database error: failed to update an invoice"
+            message: "Database error: failed to update the invoice"
         }
     }
 
